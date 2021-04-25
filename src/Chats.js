3e9f0db327158b877feeadf29796acb61feafbf3
manish-10 <mks19121999@gmail.com>
2021-04-26 03:53:07 +0530

    Added a Message Input box

--- src/Chats.js
+++ src/Chats.js
@@ -1,13 +1,21 @@
-import { Avatar, IconButton } from '@material-ui/core'
+import { Avatar, IconButton} from '@material-ui/core'
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons'
 import { useEffect, useState } from 'react'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 import './Chats.css'
 function Chat() {
     const [seed, setSeed] = useState('')
-
+    const[input,setInput]=useState('')
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
     }, [])
+
+    const sendMessage=(e)=>{
+        e.preventDefault();
+        console.log(input)
+        setInput('')
+    }
 
 
     return (
@@ -47,7 +55,13 @@ function Chat() {
 
             </div>
             <div className="chat__footer">
-
+                <InsertEmoticonIcon/>
+                <form>
+                    <input type="text" placeholder="Type a message" value={input} onChange={(e)=>setInput(e.target.value)}/>
+                    
+                    <button onClick={sendMessage}>Send a message</button>
+                </form>
+                <MicIcon/>
             </div>
         </div>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'
 const firebaseConfig = {
     apiKey: "AIzaSyACUzaBXhbwP_QaP0wevZXZU6O1csMsqw4",
     authDomain: "whatsapp-clone-d532b.firebaseapp.com",
@@ -8,3 +9,11 @@ const firebaseConfig = {
     appId: "1:690663159316:web:c78a21cd789c15ae1fd901",
     measurementId: "G-VN5809N27C"
   };
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+  const auth= firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export {auth , provider};
+  export default db;
